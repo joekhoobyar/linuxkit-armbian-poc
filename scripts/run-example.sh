88aa0ca3ed83f6f53f398bc362a3e7e576c1b844
Joe Khoobyar <fourheads@gmail.com>
2022-01-12 23:51:18 -0500

    a working qemu vexpress

--- scripts/run-example.sh
+++ scripts/run-example.sh
@@ -21,15 +21,18 @@ prefix="linuxkit-${BOARD}"
     echo -n "$UUID" > "$UUIDFILE"
   fi
 
+  cp ../ext-bin/empty-128mb.ext4 rootfs.ext4
+
   qemu-system-${ARCH} \
-    -cpu cortex-a15 -machine smdkc210 -smp 2 -m 2048 \
+    -cpu cortex-a15 -machine type=vexpress-a15 -smp 2 -m 2G \
+    -dtb ../ext-bin/vexpress-v2p-ca15-tc1.dtb \
     -uuid 60c2fbce-4e3e-4390-9241-ee425e1caefc \
     -pidfile "${STATE}/qemu.pid" \
-    -net user \
-    -nographic \
-    -kernel ${prefix}-kernel -initrd ${prefix}-initrd.img
+    -serial mon:stdio -nic user,ipv6=off,hostfwd=tcp::5555-:22 \
+    -drive if=sd,driver=file,filename=rootfs.ext4 \
+    -kernel ${prefix}-kernel -initrd ${prefix}-initrd.img \
+    -append 'console=tty0 console=ttyS0 console=ttyAMA0'
     # -device virtio-rng-pci
     # -net nic,model=virtio,macaddr=16:da:11:b4:44:c9
-    # -append 'console=tty0 console=ttyS0 console=ttyAMA0'
 )
 
